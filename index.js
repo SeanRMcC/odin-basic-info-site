2c5a64a9a313697a0a059a02f7cf031325fe9ba0
Sean <soxfansean@gmail.com>
2023-08-12 08:16:48 -0400

    Create working server that serves html & includes 404 page

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const http = require("http");
 const fs = require ("fs");
 
 const server = http.createServer((req, res) => {
-    res.end("Hello")
+    let filePath = path.join(__dirname, "routes", req.url === "/" ? "index.html" : `${req.url}.html`);
+    let contentType = { "Content-Type": "text/html" };
+
+    fs.readFile(filePath, (err, data) => {
+        if(err){
+            if(err.code === "ENOENT"){
+                const errorFile = path.join(__dirname, "routes", "404.html");
+                fs.readFile(errorFile, (err, data) => {
+                    res.writeHead(200, contentType);
+                    res.end(data, "utf8");
+                })
+            }else{
+                res.writeHead(200, contentType);
+                res.end("Server error :(");
+            }
+        }else{
+            res.writeHead(200, contentType);
+            res.end(data, "utf8");
+        }
+        
+    });
 });
 
 const PORT = 8080;
